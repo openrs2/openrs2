c6921fba9df242c2d40f52ac0e207466f26c4caf
Graham <gpe@openrs2.dev>
2019-07-29 13:52:30 +0100

    Add InsnNodeUtils
    This commit introduces four methods: two for finding the next/previous
    AbstractInsnNode that contains a real JVM opcode and two for finding the
    next/previous AbstractInsnNode that is virtual (e.g. a label).

---
+++ asm/src/main/java/dev/openrs2/asm/InsnNodeUtils.java
@@ -0,0 +1,29 @@
+package dev.openrs2.asm;
+
+import org.objectweb.asm.tree.AbstractInsnNode;
+
+public final class InsnNodeUtils {
+	public static AbstractInsnNode nextReal(AbstractInsnNode insn) {
+		while ((insn = insn.getNext()) != null && insn.getOpcode() == -1);
+		return insn;
+	}
+
+	public static AbstractInsnNode previousReal(AbstractInsnNode insn) {
+		while ((insn = insn.getPrevious()) != null && insn.getOpcode() == -1);
+		return insn;
+	}
+
+	public static AbstractInsnNode nextVirtual(AbstractInsnNode insn) {
+		while ((insn = insn.getNext()) != null && insn.getOpcode() != -1);
+		return insn;
+	}
+
+	public static AbstractInsnNode previousVirtual(AbstractInsnNode insn) {
+		while ((insn = insn.getPrevious()) != null && insn.getOpcode() != -1);
+		return insn;
+	}
+
+	private InsnNodeUtils() {
+		/* empty */
+	}
+}
