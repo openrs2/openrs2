f292bdf33ef14d5bb0f11f44b6c01201e216ed61
Graham <gpe@openrs2.dev>
2019-08-04 08:13:29 +0100

    Add jagmisc skeleton

---
+++ misc-natives/src/main/c/jagmisc.c
@@ -0,0 +1,12 @@
+#include "jagex3_jagmisc_jagmisc.h"
+
+JNIEXPORT jboolean JNICALL Java_jagex3_jagmisc_jagmisc_init(JNIEnv *env, jclass cls) {
+	return JNI_FALSE;
+}
+
+JNIEXPORT void JNICALL Java_jagex3_jagmisc_jagmisc_Quit0(JNIEnv *env, jclass cls) {
+}
+
+JNIEXPORT jlong JNICALL Java_jagex3_jagmisc_jagmisc_nanoTime(JNIEnv *env, jclass cls) {
+	return 0;
+}
