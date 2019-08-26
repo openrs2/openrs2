fbf6a5f597bc1d1679b11b97a738f7aa07f10062
Graham <gpe@openrs2.dev>
2019-08-26 09:21:49 +0100

    Stop building OS-specific modules on all systems
    They were only built on all systems for the benefit of the site plugin,
    but I'm not intending to use that any more.

--- gl-dri-natives/src/main/c/jaggl_dri.c
+++ gl-dri-natives/src/main/c/jaggl_dri.c
@@ -1,14 +1,11 @@
 #include "jaggl_X11_dri.h"
 
-#if defined(__unix__)
 #include <dlfcn.h>
 #include <stddef.h>
 
 static void *jaggl_dri_handle;
-#endif
 
 JNIEXPORT jboolean JNICALL Java_jaggl_X11_dri_open(JNIEnv *env, jclass cls, jstring path) {
-#if defined(__unix__)
 	if (jaggl_dri_handle) {
 		dlclose(jaggl_dri_handle);
 		jaggl_dri_handle = NULL;
@@ -19,19 +16,14 @@ JNIEXPORT jboolean JNICALL Java_jaggl_X11_dri_open(JNIEnv *env, jclass cls, jstr
 	(*env)->ReleaseStringUTFChars(env, path, path_str);
 
 	return jaggl_dri_handle != NULL;
-#else
-	return JNI_FALSE;
-#endif
 }
 
 JNIEXPORT jboolean JNICALL Java_jaggl_X11_dri_close(JNIEnv *env, jclass cls) {
-#if defined(__unix__)
 	if (jaggl_dri_handle) {
 		dlclose(jaggl_dri_handle);
 		jaggl_dri_handle = NULL;
 		return JNI_TRUE;
 	}
-#endif
 
 	return JNI_FALSE;
 }
