1333cc31e6afb0756a82c1937f95ad7d32df9615
Graham <gpe@openrs2.dev>
2019-09-04 09:19:12 +0100

    Format StringUtilsTest

--- util/src/test/java/dev/openrs2/util/StringUtilsTest.java
+++ util/src/test/java/dev/openrs2/util/StringUtilsTest.java
@@ -1,6 +1,7 @@
 package dev.openrs2.util;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
