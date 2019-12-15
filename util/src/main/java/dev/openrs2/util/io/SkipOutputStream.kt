429867e3af3fd9ec720b9360a4485851b294c1c1
Graham <gpe@openrs2.dev>
2019-12-15 16:59:59 +0000

    Convert SkipOutputStream to Kotlin
    This commit also changes the skipBytes logic slightly to avoid the need
    to modify the off/len arguments, which is not possible in Kotlin.

---
+++ util/src/main/java/dev/openrs2/util/io/SkipOutputStream.kt
@@ -0,0 +1,23 @@
+package dev.openrs2.util.io
+
+import java.io.FilterOutputStream
+import java.io.OutputStream
+
+class SkipOutputStream(out: OutputStream, private var skipBytes: Long) : FilterOutputStream(out) {
+    override fun write(b: Int) {
+        if (skipBytes == 0L) {
+            super.write(b)
+        } else {
+            skipBytes--
+        }
+    }
+
+    override fun write(b: ByteArray, off: Int, len: Int) {
+        if (len >= skipBytes) {
+            super.write(b, off + skipBytes.toInt(), len - skipBytes.toInt())
+            skipBytes = 0
+        } else {
+            skipBytes -= len
+        }
+    }
+}
