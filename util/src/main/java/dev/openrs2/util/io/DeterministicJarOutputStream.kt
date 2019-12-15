caaa53095d88184bcca42a792dab90e601f7faf2
Graham <gpe@openrs2.dev>
2019-12-15 16:53:44 +0000

    Convert DeterministicJarOutputStream to Kotlin

---
+++ util/src/main/java/dev/openrs2/util/io/DeterministicJarOutputStream.kt
@@ -0,0 +1,23 @@
+package dev.openrs2.util.io
+
+import java.io.OutputStream
+import java.nio.file.attribute.FileTime
+import java.util.jar.JarOutputStream
+import java.util.jar.Manifest
+import java.util.zip.ZipEntry
+
+class DeterministicJarOutputStream : JarOutputStream {
+    constructor(out: OutputStream) : super(out)
+    constructor(out: OutputStream, man: Manifest) : super(out, man)
+
+    override fun putNextEntry(ze: ZipEntry) {
+        ze.creationTime = UNIX_EPOCH
+        ze.lastAccessTime = UNIX_EPOCH
+        ze.lastModifiedTime = UNIX_EPOCH
+        super.putNextEntry(ze)
+    }
+
+    companion object {
+        private val UNIX_EPOCH = FileTime.fromMillis(0)
+    }
+}
