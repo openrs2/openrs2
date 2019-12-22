c02fe030fa20e6570d36422da58ebc5847516b5d
Graham <gpe@openrs2.dev>
2019-12-22 14:25:02 +0000

    Convert ClassForNameRemapper to Kotlin

---
+++ asm/src/main/java/dev/openrs2/asm/remap/ClassForNameRemapper.kt
@@ -0,0 +1,35 @@
+package dev.openrs2.asm.remap
+
+import dev.openrs2.asm.InsnMatcher
+import org.objectweb.asm.commons.Remapper
+import org.objectweb.asm.tree.AbstractInsnNode
+import org.objectweb.asm.tree.LdcInsnNode
+import org.objectweb.asm.tree.MethodInsnNode
+import org.objectweb.asm.tree.MethodNode
+
+object ClassForNameRemapper {
+    private val INVOKE_MATCHER = InsnMatcher.compile("LDC INVOKESTATIC")
+
+    private fun isClassForName(match: List<AbstractInsnNode>): Boolean {
+        val ldc = match[0] as LdcInsnNode
+        if (ldc.cst !is String) {
+            return false
+        }
+
+        val invokestatic = match[1] as MethodInsnNode
+        return invokestatic.owner == "java/lang/Class"
+            && invokestatic.name == "forName"
+            && invokestatic.desc == "(Ljava/lang/String;)Ljava/lang/Class;"
+    }
+
+    @JvmStatic
+    fun remap(remapper: Remapper, method: MethodNode) {
+        INVOKE_MATCHER.match(method).filter(ClassForNameRemapper::isClassForName).forEach {
+            val ldc = it[0] as LdcInsnNode
+            val name = remapper.map(ldc.cst as String)
+            if (name != null) {
+                ldc.cst = name
+            }
+        }
+    }
+}
