a2afcf341b1531301eecb85cbfb2452fe1b6ea37
Graham <gpe@openrs2.dev>
2019-12-23 13:35:32 +0000

    Convert CanvasTransformer to Kotlin

---
+++ deob/src/main/java/dev/openrs2/deob/transform/CanvasTransformer.kt
@@ -0,0 +1,26 @@
+package dev.openrs2.deob.transform
+
+import dev.openrs2.asm.classpath.ClassPath
+import dev.openrs2.asm.classpath.Library
+import dev.openrs2.asm.transform.Transformer
+import org.objectweb.asm.Opcodes
+import org.objectweb.asm.tree.ClassNode
+
+class CanvasTransformer : Transformer() {
+    public override fun transformClass(
+        classPath: ClassPath,
+        library: Library,
+        clazz: ClassNode
+    ): Boolean {
+        if (clazz.superName != "java/awt/Canvas") {
+            return false
+        }
+
+        if (clazz.access and Opcodes.ACC_FINAL == 0) {
+            return false
+        }
+
+        clazz.interfaces.remove("java/awt/event/FocusListener")
+        return false
+    }
+}
