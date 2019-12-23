77d3489b2389116cad91151e486f893449fa2bde
Graham <gpe@openrs2.dev>
2019-12-23 11:10:35 +0000

    Convert BitShiftTransformer to Kotlin

---
+++ deob/src/main/java/dev/openrs2/deob/transform/BitShiftTransformer.kt
@@ -0,0 +1,53 @@
+package dev.openrs2.deob.transform
+
+import com.github.michaelbull.logging.InlineLogger
+import dev.openrs2.asm.InsnMatcher
+import dev.openrs2.asm.InsnNodeUtils
+import dev.openrs2.asm.classpath.ClassPath
+import dev.openrs2.asm.classpath.Library
+import dev.openrs2.asm.transform.Transformer
+import org.objectweb.asm.Opcodes
+import org.objectweb.asm.tree.ClassNode
+import org.objectweb.asm.tree.MethodNode
+
+class BitShiftTransformer : Transformer() {
+    private var simplified = 0
+
+    public override fun preTransform(classPath: ClassPath) {
+        simplified = 0
+    }
+
+    public override fun transformCode(
+        classPath: ClassPath,
+        library: Library,
+        clazz: ClassNode,
+        method: MethodNode
+    ): Boolean {
+        CONST_SHIFT_MATCHER.match(method).forEach {
+            val push = it[0]
+            val bits = InsnNodeUtils.getIntConstant(push)
+
+            val opcode = it[1].opcode
+            val mask = if (LONG_SHIFTS.contains(opcode)) 63 else 31
+
+            val simplifiedBits = bits and mask
+
+            if (bits != simplifiedBits) {
+                method.instructions[push] = InsnNodeUtils.createIntConstant(simplifiedBits)
+                simplified++
+            }
+        }
+        return false
+    }
+
+    public override fun postTransform(classPath: ClassPath) {
+        logger.info { "Simplified $simplified bit shifts" }
+    }
+
+    companion object {
+        private val logger = InlineLogger()
+        private val CONST_SHIFT_MATCHER =
+            InsnMatcher.compile("(ICONST | BIPUSH | SIPUSH | LDC) (ISHL | ISHR | IUSHR | LSHL | LSHR | LUSHR)")
+        private val LONG_SHIFTS = setOf(Opcodes.LSHL, Opcodes.LSHR, Opcodes.LUSHR)
+    }
+}
