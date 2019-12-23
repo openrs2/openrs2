d6fc7b337b654bfd8e59384b8c34c9bd65d4bb2f
Graham <gpe@openrs2.dev>
2019-12-23 21:11:34 +0000

    Convert Decompiler to Kotlin

---
+++ decompiler/src/main/java/dev/openrs2/decompiler/Decompiler.kt
@@ -0,0 +1,61 @@
+package dev.openrs2.decompiler
+
+import org.jetbrains.java.decompiler.main.Fernflower
+import org.jetbrains.java.decompiler.main.extern.IFernflowerPreferences
+import java.io.Closeable
+import java.nio.file.Path
+import java.nio.file.Paths
+
+class Decompiler(
+    private val sources: List<Path>,
+    destination: (String) -> Path
+) : Closeable {
+    private val io = DecompilerIo(destination)
+    private val fernflower = Fernflower(io, io, OPTIONS, Slf4jFernflowerLogger)
+
+    fun run() {
+        for (source in sources) {
+            fernflower.addSource(source.toFile())
+        }
+        fernflower.decompileContext()
+    }
+
+    override fun close() {
+        io.close()
+    }
+
+    companion object {
+        private val OPTIONS = mapOf(
+            IFernflowerPreferences.ASCII_STRING_CHARACTERS to "1",
+            IFernflowerPreferences.INDENT_STRING to "\t",
+            IFernflowerPreferences.SYNTHETIC_NOT_SET to "1"
+        )
+
+        private fun getDestination(archive: String): Path {
+            var dir = archive.replace(Regex("(?:_gl)?[.]jar$"), "")
+            when (dir) {
+                "runescape" -> dir = "client"
+                "jaggl" -> dir = "gl"
+                "jaggl_dri" -> dir = "gl-dri"
+            }
+            return Paths.get("nonfree").resolve(dir).resolve("src/main/java")
+        }
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val deobOutput = Paths.get("nonfree/code/deob")
+            val sources = listOf(
+                deobOutput.resolve("runescape_gl.jar"),
+                deobOutput.resolve("jaggl.jar"),
+                deobOutput.resolve("jaggl_dri.jar"),
+                deobOutput.resolve("loader_gl.jar"),
+                deobOutput.resolve("signlink_gl.jar"),
+                deobOutput.resolve("unpack_gl.jar"),
+                deobOutput.resolve("unpacker_gl.jar")
+            )
+            Decompiler(sources, this::getDestination).use {
+                it.run()
+            }
+        }
+    }
+}
