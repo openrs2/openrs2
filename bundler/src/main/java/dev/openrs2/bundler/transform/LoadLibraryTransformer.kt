f4a608acafb7342655c1ce87acb20dab4e313a23
Graham <gpe@openrs2.dev>
2019-12-23 20:10:06 +0000

    Convert LoadLibraryTransformer to Kotlin

---
+++ bundler/src/main/java/dev/openrs2/bundler/transform/LoadLibraryTransformer.kt
@@ -0,0 +1,47 @@
+package dev.openrs2.bundler.transform
+
+import com.github.michaelbull.logging.InlineLogger
+import dev.openrs2.asm.InsnMatcher
+import dev.openrs2.asm.classpath.ClassPath
+import dev.openrs2.asm.classpath.Library
+import dev.openrs2.asm.transform.Transformer
+import org.objectweb.asm.tree.ClassNode
+import org.objectweb.asm.tree.LdcInsnNode
+import org.objectweb.asm.tree.MethodNode
+
+class LoadLibraryTransformer : Transformer() {
+    private var jnilibs = 0
+    private var amd64Checks = 0
+
+    override fun preTransform(classPath: ClassPath) {
+        jnilibs = 0
+        amd64Checks = 0
+    }
+
+    override fun transformCode(classPath: ClassPath, library: Library, clazz: ClassNode, method: MethodNode): Boolean {
+        val jnilib = method.instructions.find { it is LdcInsnNode && it.cst == "libjaggl.jnilib" } ?: return false
+        jnilib as LdcInsnNode
+        jnilib.cst = "libjaggl.dylib"
+        jnilibs++
+
+        AMD64_CHECK_MATCHER.match(method).forEach {
+            val ldc = it[1] as LdcInsnNode
+            if (ldc.cst == "amd64") {
+                it.forEach(method.instructions::remove)
+                amd64Checks++
+            }
+        }
+
+        return false
+    }
+
+    override fun postTransform(classPath: ClassPath) {
+        logger.info { "Replaced $jnilibs jnilibs with dylibs and removed $amd64Checks amd64 jagmisc checks" }
+    }
+
+    companion object {
+        private val logger = InlineLogger()
+        private val AMD64_CHECK_MATCHER =
+            InsnMatcher.compile("GETSTATIC LDC INVOKEVIRTUAL IFNE GETSTATIC LDC INVOKEVIRTUAL IFNE")
+    }
+}
