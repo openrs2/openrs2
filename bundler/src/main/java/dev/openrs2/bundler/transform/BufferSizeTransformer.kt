6e9a7f36ef12ae2f0ad438da3d40bd3ce716f1e7
Graham <gpe@openrs2.dev>
2019-12-23 20:43:10 +0000

    Convert RightClickTransformer to Kotlin

---
+++ bundler/src/main/java/dev/openrs2/bundler/transform/BufferSizeTransformer.kt
@@ -0,0 +1,75 @@
+package dev.openrs2.bundler.transform
+
+import com.github.michaelbull.logging.InlineLogger
+import dev.openrs2.asm.InsnMatcher
+import dev.openrs2.asm.MemberRef
+import dev.openrs2.asm.classpath.ClassPath
+import dev.openrs2.asm.classpath.Library
+import dev.openrs2.asm.createIntConstant
+import dev.openrs2.asm.hasCode
+import dev.openrs2.asm.transform.Transformer
+import org.objectweb.asm.tree.ClassNode
+import org.objectweb.asm.tree.FieldInsnNode
+import org.objectweb.asm.tree.LdcInsnNode
+import org.objectweb.asm.tree.MethodNode
+import kotlin.streams.asSequence
+
+class BufferSizeTransformer : Transformer() {
+    private var buffer: MemberRef? = null
+    private var buffersResized = 0
+
+    override fun preTransform(classPath: ClassPath) {
+        buffer = null
+        buffersResized = 0
+
+        for (library in classPath.libraries) {
+            for (clazz in library) {
+                for (method in clazz.methods) {
+                    if (!method.hasCode()) {
+                        continue
+                    }
+
+                    this.buffer = findBuffer(method) ?: continue
+                    logger.info { "Identified buffer: ${this.buffer}" }
+                    break
+                }
+            }
+        }
+    }
+
+    override fun transformCode(classPath: ClassPath, library: Library, clazz: ClassNode, method: MethodNode): Boolean {
+        if (buffer == null) {
+            return false
+        }
+
+        NEW_BUFFER_MATCHER.match(method).forEach {
+            val putstatic = it[4] as FieldInsnNode
+            if (MemberRef(putstatic) == buffer!!) {
+                method.instructions[it[2]] = createIntConstant(65535)
+                buffersResized++
+            }
+        }
+
+        return false
+    }
+
+    override fun postTransform(classPath: ClassPath) {
+        logger.info { "Resized $buffersResized buffers to 65535 bytes" }
+    }
+
+    companion object {
+        private val logger = InlineLogger()
+        private val GPP1_POS_MATCHER = InsnMatcher.compile("LDC (INVOKESPECIAL | INVOKEVIRTUAL) GETSTATIC")
+        private val NEW_BUFFER_MATCHER = InsnMatcher.compile("NEW DUP (SIPUSH | LDC) INVOKESPECIAL PUTSTATIC")
+
+        private fun findBuffer(method: MethodNode): MemberRef? {
+            return GPP1_POS_MATCHER.match(method).asSequence().filter {
+                val ldc = it[0] as LdcInsnNode
+                ldc.cst == "gpp1 pos:"
+            }.map {
+                val getstatic = it[2] as FieldInsnNode
+                MemberRef(getstatic)
+            }.firstOrNull()
+        }
+    }
+}
