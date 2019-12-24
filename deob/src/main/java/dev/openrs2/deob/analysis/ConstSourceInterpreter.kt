fdd1835fddd988a0914184e7be9ed6918d748198
Graham <gpe@openrs2.dev>
2019-12-24 19:37:54 +0000

    Convert ConstSourceInterpreter to Kotlin

---
+++ deob/src/main/java/dev/openrs2/deob/analysis/ConstSourceInterpreter.kt
@@ -0,0 +1,92 @@
+package dev.openrs2.deob.analysis
+
+import dev.openrs2.asm.intConstant
+import org.objectweb.asm.Opcodes
+import org.objectweb.asm.Type
+import org.objectweb.asm.tree.AbstractInsnNode
+import org.objectweb.asm.tree.analysis.AnalyzerException
+import org.objectweb.asm.tree.analysis.BasicInterpreter
+import org.objectweb.asm.tree.analysis.Interpreter
+
+class ConstSourceInterpreter : Interpreter<ConstSourceValue>(Opcodes.ASM7) {
+    private val basicInterpreter = BasicInterpreter()
+
+    override fun newValue(type: Type?): ConstSourceValue? {
+        val basicValue = basicInterpreter.newValue(type) ?: return null
+        return ConstSourceValue.createUnknown(basicValue)
+    }
+
+    @Throws(AnalyzerException::class)
+    override fun newOperation(insn: AbstractInsnNode): ConstSourceValue {
+        val basicValue = basicInterpreter.newOperation(insn)
+        return if (insn.intConstant != null) {
+            ConstSourceValue.createSingleSourceConstant(basicValue, insn)
+        } else {
+            ConstSourceValue.createUnknown(basicValue)
+        }
+    }
+
+    @Throws(AnalyzerException::class)
+    override fun copyOperation(insn: AbstractInsnNode, value: ConstSourceValue): ConstSourceValue {
+        val basicValue = basicInterpreter.copyOperation(insn, value.basicValue)
+        return ConstSourceValue.createUnknown(basicValue)
+    }
+
+    @Throws(AnalyzerException::class)
+    override fun unaryOperation(insn: AbstractInsnNode, value: ConstSourceValue): ConstSourceValue? {
+        val basicValue = basicInterpreter.unaryOperation(insn, value.basicValue) ?: return null
+        return ConstSourceValue.createUnknown(basicValue)
+    }
+
+    @Throws(AnalyzerException::class)
+    override fun binaryOperation(
+        insn: AbstractInsnNode,
+        value1: ConstSourceValue,
+        value2: ConstSourceValue
+    ): ConstSourceValue? {
+        val basicValue =
+            basicInterpreter.binaryOperation(insn, value1.basicValue, value2.basicValue)
+                ?: return null
+        return ConstSourceValue.createUnknown(basicValue)
+    }
+
+    @Throws(AnalyzerException::class)
+    override fun ternaryOperation(
+        insn: AbstractInsnNode,
+        value1: ConstSourceValue,
+        value2: ConstSourceValue,
+        value3: ConstSourceValue
+    ): ConstSourceValue? {
+        val basicValue = basicInterpreter.ternaryOperation(
+            insn,
+            value1.basicValue,
+            value2.basicValue,
+            value3.basicValue
+        ) ?: return null
+        return ConstSourceValue.createUnknown(basicValue)
+    }
+
+    @Throws(AnalyzerException::class)
+    override fun naryOperation(
+        insn: AbstractInsnNode,
+        values: List<ConstSourceValue>
+    ): ConstSourceValue? {
+        val args = values.map { it.basicValue }.toList()
+        val basicValue = basicInterpreter.naryOperation(insn, args) ?: return null
+        return ConstSourceValue.createUnknown(basicValue)
+    }
+
+    @Throws(AnalyzerException::class)
+    override fun returnOperation(insn: AbstractInsnNode, value: ConstSourceValue, expected: ConstSourceValue) {
+        basicInterpreter.returnOperation(insn, value.basicValue, expected.basicValue)
+    }
+
+    override fun merge(value1: ConstSourceValue, value2: ConstSourceValue): ConstSourceValue {
+        val basicValue = basicInterpreter.merge(value1.basicValue, value2.basicValue)
+        return if (value1.isSingleSourceConstant && value1 == value2) {
+            value1
+        } else {
+            ConstSourceValue.createUnknown(basicValue)
+        }
+    }
+}
