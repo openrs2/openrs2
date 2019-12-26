320a8a7f8a67956700e43402b2872869be019605
Graham <gpe@openrs2.dev>
2019-12-26 19:19:00 +0000

    Convert NewInstanceTransformer to Kotlin

---
+++ deob-ast/src/main/java/dev/openrs2/deob/ast/transform/NewInstanceTransformer.kt
@@ -0,0 +1,22 @@
+package dev.openrs2.deob.ast.transform
+
+import com.github.javaparser.ast.CompilationUnit
+import com.github.javaparser.ast.Node
+import com.github.javaparser.ast.expr.MethodCallExpr
+import dev.openrs2.deob.ast.util.NodeUtils
+
+class NewInstanceTransformer : Transformer() {
+    override fun transform(unit: CompilationUnit) {
+        NodeUtils.walk(unit, Node.TreeTraversal.POSTORDER, MethodCallExpr::class.java) { expr ->
+            if (expr.nameAsString != "newInstance") {
+                return@walk
+            }
+
+            expr.scope.ifPresent { scope ->
+                if (!scope.isMethodCallExpr || scope.asMethodCallExpr().nameAsString != "getConstructor") {
+                    expr.setScope(MethodCallExpr(scope.clone(), "getDeclaredConstructor"))
+                }
+            }
+        }
+    }
+}
