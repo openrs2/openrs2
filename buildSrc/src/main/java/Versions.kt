deb65474c7eb87ffc4d8b2e8926e91b6372e6f32
Graham <gpe@openrs2.dev>
2020-02-28 19:40:10 +0000

    Update dependencies

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -12,7 +12,7 @@ object Versions {
     const val kotlin = "1.3.61"
     const val kotlinter = "2.3.1"
     const val logback = "1.2.3"
-    const val netty = "4.1.45.Final"
+    const val netty = "4.1.46.Final"
     const val openrs2Natives = "2.0.0"
     const val shadowPlugin = "5.2.0"
     const val versionsPlugin = "0.28.0"
