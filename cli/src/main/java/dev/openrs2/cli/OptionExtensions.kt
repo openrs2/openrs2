231e0893af44574b3d9f0891eb6655d4f2c4956f
Graham <gpe@openrs2.dev>
2020-04-03 21:07:45 +0100

    Add java.nio.file.Paths import
    Annoyingly a bunch of IDEA's refactoring tools seem to cause imports to
    disappear.
    
    Signed-off-by: Graham <gpe@openrs2.dev>

--- cli/src/main/java/dev/openrs2/cli/OptionExtensions.kt
+++ cli/src/main/java/dev/openrs2/cli/OptionExtensions.kt
@@ -8,13 +8,14 @@ import com.github.ajalt.clikt.parameters.options.default
 import java.io.InputStream
 import java.io.OutputStream
 import java.nio.file.Files
+import java.nio.file.Paths
 
 fun RawOption.inputStream(): NullableOption<InputStream, InputStream> {
     return convert("FILE") {
         return@convert if (it == "-") {
             System.`in`
         } else {
-            Files.newInputStream(java.nio.file.Paths.get(it))
+            Files.newInputStream(Paths.get(it))
         }
     }
 }
@@ -28,7 +29,7 @@ fun RawOption.outputStream(): NullableOption<OutputStream, OutputStream> {
         return@convert if (it == "-") {
             System.out
         } else {
-            Files.newOutputStream(java.nio.file.Paths.get(it))
+            Files.newOutputStream(Paths.get(it))
         }
     }
 }
