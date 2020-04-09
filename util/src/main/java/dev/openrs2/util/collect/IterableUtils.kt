09f17d246bd19f84132ac667fb9d69a8c8dcf027
Graham <gpe@openrs2.dev>
2020-04-09 20:01:42 +0100

    Add additional removeFirst() variants to IterableUtils
    Signed-off-by: Graham <gpe@openrs2.dev>

--- util/src/main/java/dev/openrs2/util/collect/IterableUtils.kt
+++ util/src/main/java/dev/openrs2/util/collect/IterableUtils.kt
@@ -1,5 +1,21 @@
 package dev.openrs2.util.collect
 
+fun <T> MutableIterable<T>.removeFirst(): T {
+    return removeFirstOrNull() ?: throw NoSuchElementException()
+}
+
+fun <T> MutableIterable<T>.removeFirstOrNull(): T? {
+    val iterator = iterator()
+    if (!iterator.hasNext()) {
+        return null
+    }
+
+    val element = iterator.next()
+    iterator.remove()
+
+    return element
+}
+
 inline fun <T> MutableIterable<T>.removeFirst(predicate: (T) -> Boolean): Boolean {
     val iterator = iterator()
     for (element in iterator) {
