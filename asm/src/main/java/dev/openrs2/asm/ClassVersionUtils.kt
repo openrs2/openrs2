bbe41571246a6f7e03dfb0c8dea4de261093f2fe
Graham <gpe@openrs2.dev>
2020-04-12 20:17:51 +0100

    Add gte() method to ClassVersionUtils

--- asm/src/main/java/dev/openrs2/asm/ClassVersionUtils.kt
+++ asm/src/main/java/dev/openrs2/asm/ClassVersionUtils.kt
@@ -5,8 +5,12 @@ object ClassVersionUtils {
         return (v shl 16) or (v ushr 16)
     }
 
+    fun gte(v1: Int, v2: Int): Boolean {
+        return swapWords(v1) >= swapWords(v2)
+    }
+
     fun maxVersion(v1: Int, v2: Int): Int {
-        return if (swapWords(v1) > swapWords(v2)) {
+        return if (gte(v1, v2)) {
             v1
         } else {
             v2
