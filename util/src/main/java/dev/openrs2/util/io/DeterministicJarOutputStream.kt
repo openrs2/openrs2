0206bbd4b2eca160079c4fab85fe3df8d16de05c
Graham <gpe@openrs2.dev>
2020-05-02 22:19:48 +0100

    Set DeterministicJarOutputStream's default compression level to 9
    Signed-off-by: Graham <gpe@openrs2.dev>

--- util/src/main/java/dev/openrs2/util/io/DeterministicJarOutputStream.kt
+++ util/src/main/java/dev/openrs2/util/io/DeterministicJarOutputStream.kt
@@ -4,11 +4,16 @@ import java.io.OutputStream
 import java.nio.file.attribute.FileTime
 import java.util.jar.JarOutputStream
 import java.util.jar.Manifest
+import java.util.zip.Deflater
 import java.util.zip.ZipEntry
 
 class DeterministicJarOutputStream : JarOutputStream {
     constructor(out: OutputStream) : super(out)
     constructor(out: OutputStream, man: Manifest) : super(out, man)
+
+    init {
+        setLevel(Deflater.BEST_COMPRESSION)
+    }
 
     override fun putNextEntry(ze: ZipEntry) {
         ze.creationTime = UNIX_EPOCH
