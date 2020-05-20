69a27e43d427b2f4905ef1b3126a5347cc9ffbfd
Graham <gpe@openrs2.dev>
2020-05-20 20:44:28 +0100

    Move OpenGL registry to the share/deob folder
    Signed-off-by: Graham <gpe@openrs2.dev>

--- deob-ast/src/main/java/dev/openrs2/deob/ast/gl/GlRegistryProvider.kt
+++ deob-ast/src/main/java/dev/openrs2/deob/ast/gl/GlRegistryProvider.kt
@@ -1,15 +1,17 @@
 package dev.openrs2.deob.ast.gl
 
+import java.nio.file.Files
+import java.nio.file.Paths
 import javax.inject.Provider
 
 class GlRegistryProvider : Provider<GlRegistry> {
     override fun get(): GlRegistry {
-        GlRegistryProvider::class.java.getResourceAsStream(PATH).use { input ->
+        Files.newInputStream(PATH).use { input ->
             return GlRegistry.parse(input)
         }
     }
 
     companion object {
-        private const val PATH = "/dev/openrs2/deob/ast/gl.xml"
+        private val PATH = Paths.get("share/deob/gl.xml")
     }
 }
