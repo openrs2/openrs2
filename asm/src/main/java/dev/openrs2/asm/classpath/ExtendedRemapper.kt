1a41982b76054d2480dadb5c44ea26fd9e890e8e
Graham <gpe@openrs2.dev>
2020-05-26 18:00:36 +0100

    Rename desc to descriptor in ExtendedRemapper
    This is consistent with the map{Field,Method}Name argument names.
    
    Signed-off-by: Graham <gpe@openrs2.dev>

--- asm/src/main/java/dev/openrs2/asm/classpath/ExtendedRemapper.kt
+++ asm/src/main/java/dev/openrs2/asm/classpath/ExtendedRemapper.kt
@@ -3,11 +3,11 @@ package dev.openrs2.asm.classpath
 import org.objectweb.asm.commons.Remapper
 
 abstract class ExtendedRemapper : Remapper() {
-    open fun mapFieldOwner(owner: String, name: String, desc: String): String {
+    open fun mapFieldOwner(owner: String, name: String, descriptor: String): String {
         return mapType(owner)
     }
 
-    open fun mapMethodOwner(owner: String, name: String, desc: String): String {
+    open fun mapMethodOwner(owner: String, name: String, descriptor: String): String {
         return mapType(owner)
     }
 }
