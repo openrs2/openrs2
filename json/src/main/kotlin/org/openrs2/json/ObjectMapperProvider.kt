0a92ce020df3969ea0021526db3ecc64ee7da015
Graham <gpe@openrs2.org>
2020-12-13 14:33:07 +0000

    Fix warning about deprecated SNAKE_CASE constant
    Signed-off-by: Graham <gpe@openrs2.org>

--- json/src/main/kotlin/org/openrs2/json/ObjectMapperProvider.kt
+++ json/src/main/kotlin/org/openrs2/json/ObjectMapperProvider.kt
@@ -2,7 +2,7 @@ package org.openrs2.json
 
 import com.fasterxml.jackson.databind.Module
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.PropertyNamingStrategy
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import javax.inject.Inject
 import javax.inject.Provider
@@ -14,6 +14,6 @@ public class ObjectMapperProvider @Inject constructor(
         return ObjectMapper()
             .registerKotlinModule()
             .registerModules(modules)
-            .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
+            .setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)
     }
 }
--- yaml/src/main/kotlin/org/openrs2/yaml/ObjectMapperProvider.kt
+++ yaml/src/main/kotlin/org/openrs2/yaml/ObjectMapperProvider.kt
@@ -2,7 +2,7 @@ package org.openrs2.yaml
 
 import com.fasterxml.jackson.databind.Module
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.PropertyNamingStrategy
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import javax.inject.Inject
@@ -15,6 +15,6 @@ public class ObjectMapperProvider @Inject constructor(
         return ObjectMapper(YAMLFactory())
             .registerKotlinModule()
             .registerModules(modules)
-            .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
+            .setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)
     }
 }
