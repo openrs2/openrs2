9e983135e248b91b8613316e911b8d69566c8123
Graham <gpe@openrs2.org>
2020-12-25 19:02:47 +0000

    Make InitJs5RemoteConnection a data class
    Signed-off-by: Graham <gpe@openrs2.org>

--- protocol/src/main/kotlin/org/openrs2/protocol/login/LoginRequest.kt
+++ protocol/src/main/kotlin/org/openrs2/protocol/login/LoginRequest.kt
@@ -3,5 +3,5 @@ package org.openrs2.protocol.login
 import org.openrs2.protocol.Packet
 
 public sealed class LoginRequest : Packet {
-    public class InitJs5RemoteConnection(public val version: Int) : LoginRequest()
+    public data class InitJs5RemoteConnection(public val version: Int) : LoginRequest()
 }
