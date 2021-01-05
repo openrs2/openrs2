186c5930a560621c96b0b832b3d36149f685fe96
Graham <gpe@openrs2.org>
2021-01-05 21:00:52 +0000

    Add Js5Archive and Js5ConfigGroup constants for 550
    Most of these names come directly from the NXT/TFU clients, though a few
    are made up as we don't know the official name.
    
    Signed-off-by: Graham <gpe@openrs2.org>

--- cache/src/main/kotlin/org/openrs2/cache/Js5Archive.kt
+++ cache/src/main/kotlin/org/openrs2/cache/Js5Archive.kt
@@ -1,5 +1,34 @@
 package org.openrs2.cache
 
 public object Js5Archive {
+    public const val ANIMS: Int = 0
+    public const val BASES: Int = 1
+    public const val CONFIG: Int = 2
+    public const val INTERFACES: Int = 3
+    public const val SYNTH_SOUNDS: Int = 4
+    public const val MAPS: Int = 5
+    public const val MIDI_SONGS: Int = 6
+    public const val MODELS: Int = 7
+    public const val SPRITES: Int = 8
+    public const val TEXTURES: Int = 9
+    public const val BINARY: Int = 10
+    public const val MIDI_JINGLES: Int = 11
+    public const val CLIENTSCRIPTS: Int = 12
+    public const val FONTMETRICS: Int = 13
+    public const val VORBIS: Int = 14
+    public const val MIDI_INSTRUMENTS: Int = 15
+    public const val CONFIG_LOC: Int = 16
+    public const val CONFIG_ENUM: Int = 17
+    public const val CONFIG_NPC: Int = 18
+    public const val CONFIG_OBJ: Int = 19
+    public const val CONFIG_SEQ: Int = 20
+    public const val CONFIG_SPOT: Int = 21
+    public const val CONFIG_VAR_BIT: Int = 22
+    public const val WORLDMAPDATA: Int = 23
+    public const val QUICKCHAT: Int = 24
+    public const val QUICKCHAT_GLOBAL: Int = 25
+    public const val MATERIALS: Int = 26
+    public const val CONFIG_PARTICLE: Int = 27
+    public const val DEFAULTS: Int = 28
     public const val ARCHIVESET: Int = 255
 }
--- cache/src/main/kotlin/org/openrs2/cache/Js5ConfigGroup.kt
+++ cache/src/main/kotlin/org/openrs2/cache/Js5ConfigGroup.kt
@@ -1,3 +1,18 @@
 package org.openrs2.cache
 
-public object Js5ConfigGroup
+public object Js5ConfigGroup {
+    public const val FLUTYPE: Int = 1
+    public const val IDKTYPE: Int = 3
+    public const val FLOTYPE: Int = 4
+    public const val INVTYPE: Int = 5
+    public const val PARAMTYPE: Int = 11
+    public const val VAR_PLAYER: Int = 16
+    public const val STRUCTTYPE: Int = 26
+    public const val SKYBOXTYPE: Int = 29
+    public const val SKYBOXSPHERETYPE: Int = 30
+    public const val LIGHTTYPE: Int = 31
+    public const val BASTYPE: Int = 32
+    public const val CURSORTYPE: Int = 33
+    public const val MSITYPE: Int = 34
+    public const val MELTYPE: Int = 36
+}
