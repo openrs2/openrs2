b5516982a33be92433366dd20f6e29f0cbe263bf
Graham <gpe@openrs2.org>
2021-01-28 23:19:17 +0000

    Update dependencies
    Signed-off-by: Graham <gpe@openrs2.org>

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -8,7 +8,7 @@ object Versions {
     const val dokka = "1.4.20"
     const val fastutil = "8.4.4"
     const val fernflower = "1.1.0"
-    const val flyway = "7.5.1"
+    const val flyway = "7.5.2"
     const val guava = "30.1-jre"
     const val guice = "5.0.0-BETA-1"
     const val h2 = "1.4.200"
