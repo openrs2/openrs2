177376f47e5b254bffe546d226408837e2161559
Graham <gpe@openrs2.org>
2021-02-09 19:02:35 +0000

    Update dependencies
    Signed-off-by: Graham <gpe@openrs2.org>

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -24,7 +24,7 @@ object Versions {
     const val kotlinter = "3.3.0"
     const val ktor = "1.5.1"
     const val logback = "1.2.3"
-    const val netty = "4.1.58.Final"
+    const val netty = "4.1.59.Final"
     const val nettyIoUring = "0.0.3.Final"
     const val openrs2Natives = "3.2.0"
     const val postgres = "42.2.18"
