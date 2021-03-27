a02eb169f4efdd5f601b13ceaa7f5ff1f2a22e0e
Graham <gpe@openrs2.org>
2021-03-27 18:10:49 +0000

    Fix NPE in TextKeyReader if the input is invalid
    Signed-off-by: Graham <gpe@openrs2.org>

--- archive/src/main/kotlin/org/openrs2/archive/key/TextKeyReader.kt
+++ archive/src/main/kotlin/org/openrs2/archive/key/TextKeyReader.kt
@@ -7,10 +7,10 @@ public object TextKeyReader : KeyReader {
     override fun read(input: InputStream): Sequence<XteaKey> {
         val reader = input.bufferedReader()
 
-        val k0 = reader.readLine().toIntOrNull() ?: return emptySequence()
-        val k1 = reader.readLine().toIntOrNull() ?: return emptySequence()
-        val k2 = reader.readLine().toIntOrNull() ?: return emptySequence()
-        val k3 = reader.readLine().toIntOrNull() ?: return emptySequence()
+        val k0 = reader.readLine()?.toIntOrNull() ?: return emptySequence()
+        val k1 = reader.readLine()?.toIntOrNull() ?: return emptySequence()
+        val k2 = reader.readLine()?.toIntOrNull() ?: return emptySequence()
+        val k3 = reader.readLine()?.toIntOrNull() ?: return emptySequence()
 
         return sequenceOf(XteaKey(k0, k1, k2, k3))
     }
