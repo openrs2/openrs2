4d5c9153ab5b4fdf6ff66c1e5c26fa0aba4f2aae
Graham <gpe@openrs2.org>
2021-05-16 13:17:18 +0100

    Add support for converting a Netty Future to a CompletableFuture
    Signed-off-by: Graham <gpe@openrs2.org>

--- net/src/main/kotlin/org/openrs2/net/FutureExtensions.kt
+++ net/src/main/kotlin/org/openrs2/net/FutureExtensions.kt
@@ -1,6 +1,7 @@
 package org.openrs2.net
 
 import io.netty.util.concurrent.Future
+import java.util.concurrent.CompletableFuture
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
@@ -23,4 +24,26 @@ public suspend fun <V> Future<V>.awaitSuspend(): V {
             }
         }
     }
+}
+
+public fun <V> Future<V>.asCompletableFuture(): CompletableFuture<V> {
+    if (isDone) {
+        return if (isSuccess) {
+            CompletableFuture.completedFuture(now)
+        } else {
+            CompletableFuture.failedFuture(cause())
+        }
+    }
+
+    val future = CompletableFuture<V>()
+
+    addListener {
+        if (isSuccess) {
+            future.complete(now)
+        } else {
+            future.completeExceptionally(cause())
+        }
+    }
+
+    return future
 }
