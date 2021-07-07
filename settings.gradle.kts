18012a5f3869ee35ea93ee7cf2daada0d28e6235
Graham <gpe@openrs2.org>
2021-07-07 23:29:59 +0100

    Update dependencies
    Signed-off-by: Graham <gpe@openrs2.org>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,7 +15,7 @@ pluginManagement {
         id("com.github.ben-manes.versions") version "0.39.0"
         id("com.github.jk1.dependency-license-report") version "1.16"
         id("com.github.johnrengelman.shadow") version "7.0.0"
-        id("org.jetbrains.dokka") version "1.4.32"
+        id("org.jetbrains.dokka") version "1.5.0"
         id("org.jmailen.kotlinter") version "3.4.5"
         kotlin("jvm") version "1.5.20"
     }
