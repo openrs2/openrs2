e5512cbdf6f40f4b194f4c6eaa069113e322a127
Graham <gpe@openrs2.org>
2022-04-26 04:53:15 +0100

    Update number of loading requirements
    Signed-off-by: Graham <gpe@openrs2.org>

--- archive/src/main/kotlin/org/openrs2/archive/cache/nxt/LoginResponse.kt
+++ archive/src/main/kotlin/org/openrs2/archive/cache/nxt/LoginResponse.kt
@@ -5,7 +5,7 @@ import org.openrs2.protocol.Packet
 public sealed class LoginResponse : Packet {
     public data class Js5Ok(val loadingRequirements: List<Int>) : LoginResponse() {
         public companion object {
-            public const val LOADING_REQUIREMENTS: Int = 31
+            public const val LOADING_REQUIREMENTS: Int = 32
         }
     }
 
