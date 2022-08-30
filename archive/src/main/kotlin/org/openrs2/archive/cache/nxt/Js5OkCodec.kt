d0a46dc5e52bdd9a85f35f4094f0104987f13069
Graham <gpe@openrs2.org>
2022-08-30 17:57:17 +0100

    Removing loading requirements from the NXT downloader
    Signed-off-by: Graham <gpe@openrs2.org>

--- archive/src/main/kotlin/org/openrs2/archive/cache/nxt/Js5OkCodec.kt
+++ archive/src/main/kotlin/org/openrs2/archive/cache/nxt/Js5OkCodec.kt
@@ -1,25 +1,8 @@
 package org.openrs2.archive.cache.nxt
 
-import io.netty.buffer.ByteBuf
-import org.openrs2.crypto.StreamCipher
-import org.openrs2.protocol.FixedPacketCodec
+import org.openrs2.protocol.EmptyPacketCodec
 
-public object Js5OkCodec : FixedPacketCodec<LoginResponse.Js5Ok>(
-    type = LoginResponse.Js5Ok::class.java,
+public object Js5OkCodec : EmptyPacketCodec<LoginResponse.Js5Ok>(
     opcode = 0,
-    length = LoginResponse.Js5Ok.LOADING_REQUIREMENTS * 4
-) {
-    override fun decode(input: ByteBuf, cipher: StreamCipher): LoginResponse.Js5Ok {
-        val loadingRequirements = mutableListOf<Int>()
-        for (i in 0 until LoginResponse.Js5Ok.LOADING_REQUIREMENTS) {
-            loadingRequirements += input.readInt()
-        }
-        return LoginResponse.Js5Ok(loadingRequirements)
-    }
-
-    override fun encode(input: LoginResponse.Js5Ok, output: ByteBuf, cipher: StreamCipher) {
-        for (requirement in input.loadingRequirements) {
-            output.writeInt(requirement)
-        }
-    }
-}
+    packet = LoginResponse.Js5Ok
+)
--- archive/src/main/kotlin/org/openrs2/archive/cache/nxt/LoginResponse.kt
+++ archive/src/main/kotlin/org/openrs2/archive/cache/nxt/LoginResponse.kt
@@ -3,11 +3,6 @@ package org.openrs2.archive.cache.nxt
 import org.openrs2.protocol.Packet
 
 public sealed class LoginResponse : Packet {
-    public data class Js5Ok(val loadingRequirements: List<Int>) : LoginResponse() {
-        public companion object {
-            public const val LOADING_REQUIREMENTS: Int = 32
-        }
-    }
-
+    public object Js5Ok : LoginResponse()
     public object ClientOutOfDate : LoginResponse()
 }
