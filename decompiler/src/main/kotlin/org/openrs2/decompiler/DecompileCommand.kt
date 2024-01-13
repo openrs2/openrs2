95af2a4e99d53acf2053196d734238174d47f7e3
Pazaz <pazaz@itspazaz.com>
2024-01-13 04:09:46 -0500

    feat: Load Module list from profile

--- decompiler/src/main/kotlin/org/openrs2/decompiler/DecompileCommand.kt
+++ decompiler/src/main/kotlin/org/openrs2/decompiler/DecompileCommand.kt
@@ -7,7 +7,7 @@ public fun main(args: Array<String>): Unit = DecompileCommand().main(args)
 
 public class DecompileCommand : CliktCommand(name = "decompile") {
     override fun run() {
-        val decompiler = Decompiler()
-        decompiler.run(Module.ALL)
+        // val decompiler = Decompiler()
+        // decompiler.run(Module.ALL)
     }
 }
