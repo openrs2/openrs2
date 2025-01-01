ec50a5324f482ecad613680c6596457ceee4a142
Graham <gpe@openrs2.org>
2025-01-01 16:09:57 +0000

    Fix Postgres 17 compatibility
    Signed-off-by: Graham <gpe@openrs2.org>

---
+++ archive/src/main/resources/org/openrs2/archive/migrations/V29__search_path.sql
@@ -0,0 +1,7 @@
+-- @formatter:off
+
+ALTER FUNCTION group_blocks SET search_path = public, pg_catalog, pg_temp;
+ALTER FUNCTION resolve_archive SET search_path = public, pg_catalog, pg_temp;
+ALTER FUNCTION resolve_file SET search_path = public, pg_catalog, pg_temp;
+ALTER FUNCTION resolve_group SET search_path = public, pg_catalog, pg_temp;
+ALTER FUNCTION resolve_index SET search_path = public, pg_catalog, pg_temp;
