d92a3acc30335ece25b4e11b8f25ba730cbca8cb
Graham <gpe@openrs2.org>
2026-05-19 21:27:09 +0100

    Bump version to 0.2.0-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ plugins {
 
 allprojects {
     group = "org.openrs2"
-    version = "0.1.0"
+    version = "0.2.0-SNAPSHOT"
 
     plugins.withType<BasePlugin> {
         configure<BasePluginExtension> {
