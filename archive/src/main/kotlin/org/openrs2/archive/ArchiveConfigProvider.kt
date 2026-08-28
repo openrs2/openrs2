1745dbd9cab0a32941ae6ced0a8238196256c139
Graham <gpe@openrs2.org>
2026-08-28 12:38:25 +0100

    Make the openrs2 archive command auto-detect the config path
    Signed-off-by: Graham <gpe@openrs2.org>

--- archive/src/main/kotlin/org/openrs2/archive/ArchiveConfigProvider.kt
+++ archive/src/main/kotlin/org/openrs2/archive/ArchiveConfigProvider.kt
@@ -4,20 +4,52 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import jakarta.inject.Inject
 import jakarta.inject.Provider
 import org.openrs2.yaml.Yaml
-import java.nio.file.Files
+import java.nio.file.FileSystemNotFoundException
 import java.nio.file.Path
+import kotlin.io.path.bufferedReader
+import kotlin.io.path.copyTo
+import kotlin.io.path.notExists
+import kotlin.io.path.toPath
 
 public class ArchiveConfigProvider @Inject constructor(
     @param:Yaml private val mapper: ObjectMapper
 ) : Provider<ArchiveConfig> {
     override fun get(): ArchiveConfig {
-        if (Files.notExists(CONFIG_PATH)) {
-            Files.copy(EXAMPLE_CONFIG_PATH, CONFIG_PATH)
-        }
-
-        return Files.newBufferedReader(CONFIG_PATH).use { reader ->
+        return getConfigPath().bufferedReader().use { reader ->
             mapper.readValue(reader, ArchiveConfig::class.java)
         }
+    }
+
+    private fun getRoot(): Path {
+        // find the location of openrs2.jar
+        val codeSource = ArchiveConfigProvider::class.java.protectionDomain.codeSource
+        if (codeSource != null) {
+            val path = try {
+                codeSource.location.toURI().toPath()
+            } catch (_: FileSystemNotFoundException) {
+                null
+            }
+
+            // go from <root>/lib/openrs2.jar to <root>
+            val root = path?.parent?.parent
+            if (root != null) {
+                return root
+            }
+        }
+
+        // fall back to current working directory
+        return Path.of("")
+    }
+
+    private fun getConfigPath(): Path {
+        val root = getRoot()
+        val path = root.resolve(CONFIG_PATH)
+
+        if (path.notExists()) {
+            root.resolve(EXAMPLE_CONFIG_PATH).copyTo(path)
+        }
+
+        return path
     }
 
     private companion object {
